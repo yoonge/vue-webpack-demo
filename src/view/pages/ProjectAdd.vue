d51b4bfe8a65c64ebc7ec8d6895c0777dd3238d3
Yoonge <7884008@qq.com>
2016-05-16 19:41:22 +0800

    Update page ProjectAdd

--- src/view/pages/ProjectAdd.vue
+++ src/view/pages/ProjectAdd.vue
@@ -3,7 +3,7 @@
     <div class="mytemp">
       <div class="temp_con pd1 clear">
         <dl class="ddmb15">
-          <dd>
+          <dd class="clearfix">
             <span class="tmp-colname">任务类型</span>
             <select class="lfselect jr_select" v-model="task_type">
               <option value="0" selected>域名任务</option>
@@ -15,10 +15,17 @@
             </select>
             <span class="tmp-notice">会对该域名及该域名的子域名相关业务流量进行分析和风险检测</span>
           </dd>
-          <dd>
-            <span class="tmp-colname">目标地址</span><input type="text" class="inputControl w270"><span class="tmp-notice">与任务类型相匹配的目标地址</span>
+          <dd class="clearfix">
+            <span class="tmp-colname">目标地址</span>
+            <input type="text" class="form-control" v-if="task_type=='0'">
+            <textarea class="form-control" rows="5" v-if="task_type=='1'"></textarea>
+            <input type="text" class="form-control" v-if="task_type=='2'">
+            <textarea class="form-control" rows="5" v-if="task_type=='3'"></textarea>
+            <input type="text" class="form-control" v-if="task_type=='4'">
+            <textarea class="form-control" rows="5" v-if="task_type=='5'"></textarea>
+            <span class="tmp-notice">与任务类型相匹配的目标地址</span>
           </dd>
-          <dd>
+          <dd class="clearfix">
             <span class="tmp-colname">检测模式</span>
             <select class="lfselect jr_select onmodel" v-model="scan_mode">
               <option value="0" selected>默认模式</option>
@@ -29,53 +36,43 @@
 
         <div class="linewrap2" v-if="scan_mode=='1'">
           <dl class="ddmb15">
-            <dd>
+            <dd class="clearfix">
               <span class="tmp-colname">开启 COOKIE</span>
               <span class="myrediowrap">
                 <label><input type="radio" name="cookie" value="0" checked>是</label>
                 <label><input type="radio" name="cookie" value="1">否</label>
-                <!--<span class="myredio" rel="1"><i class="ic_kuang"></i>是</span>-->
-                <!--<span class="myredio" rel="0"><i class="ic_kuang"></i>否</span>-->
               </span>
             </dd>
-            <dd>
+            <dd class="clearfix">
               <span class="tmp-colname">扫描启动时间</span>
-              <select class="lfselect zy_select" v-model="start_time">
+              <select class="lfselect jr_select" v-model="start_time">
                 <option value="0" selected>立即启动</option>
                 <option value="1">定时启动</option>
               </select>
-              <input type="text" class="inputControl w160 inputstyle my_dsqd" placeholder="2015-01-02  12:22:11" v-if="start_time=='1'">
+              <input type="text" class="form-control" placeholder="2015-01-02  12:22:11" v-if="start_time=='1'">
               <span class="tmp-notice">您可以先启动任务来记录流量，基于流量的风险检测会在设置的时间开始进行检测</span>
             </dd>
-            <dd>
+            <dd class="clearfix">
               <span class="tmp-colname">选择插件</span>
               <span class="mycheckwrap">
-                <label><input type="checkbox">全选</label>
-                <label><input type="checkbox" v-model="checkedBoxes">SQL 注入检测</label>
-                <label><input type="checkbox" v-model="checkedBoxes">XSS 漏洞检测</label>
-                <label><input type="checkbox" v-model="checkedBoxes">命令执行漏洞</label>
-                <label><input type="checkbox" v-model="checkedBoxes">其他第三方漏洞</label>
-                <!--<span class="myredio1"><i class="ic_kuang"></i>全选</span>-->
-                <!--<span class="myredio"><i class="ic_kuang"></i>SQL注入检测</span>-->
-                <!--<span class="myredio"><i class="ic_kuang"></i>XSS漏洞检测</span>-->
-                <!--<span class="myredio"><i class="ic_kuang"></i>命令执行漏洞</span>-->
-                <!--<span class="myredio"><i class="ic_kuang"></i>其他第三方漏洞</span>-->
+                <label><input type="checkbox" v-model="checkAll">全选</label>
+                <label v-for="checkBox in checkBoxes"><input type="checkbox" v-model="checkBox.checked">{{checkBox.name}}</label>
               </span>
             </dd>
-            <dd class="zdy_wrap fl ld">
+            <dd class="zdy_wrap fl ld clearfix">
               <span class="tmp-colname fl">自定义请求头</span>
-              <dl class="fl wrapper_xz">
-                <dd class="zdy_linehead">
-                  <input type="text" class="inputControl inputstyle" placeholder="字段名"><span class="zdy_mh">:</span><input type="text" class="inputControl inputstyle w160" placeholder="内容">
+              <dl class="fl">
+                <dd class="zdy_linehead clearfix">
+                  <input type="text" class="form-control" placeholder="字段名"><span class="zdy_mh">:</span><input type="text" class="form-control" placeholder="内容">
                 </dd>
               </dl>
-              <button class="xz_btn color2 addyx">+ 新增一项</button>
+              <a href="javascript:;" class="mybtn">+ 新增一项</a>
             </dd>
           </dl>
         </div>
 
         <div class="btn-div">
-          <button class="blue ml127">提交</button>
+          <button class="blue">提交</button>
         </div>
 
       </div>
@@ -91,7 +88,46 @@ export default {
       task_type: '',
       scan_mode: '',
       start_time: '',
-      checkedBoxes: []
+      checkBoxes: [
+        {
+          name: 'SQL 注入检测',
+          checked: false
+        },
+        {
+          name: 'XSS 漏洞检测',
+          checked: false
+        },
+        {
+          name: '命令执行漏洞',
+          checked: false
+        },
+        {
+          name: '其他第三方漏洞',
+          checked: false
+        }
+      ]
+    }
+  },
+  computed: {
+    checkAll: {
+      get: function () {
+        return this.checkedCount === this.checkBoxes.length
+      },
+      set: function (val) {
+        this.checkBoxes.forEach(function (item) {
+          item.checked = val
+        })
+        return val
+      }
+    },
+    checkedCount: {
+      get: function () {
+        let i = 0
+        this.checkBoxes.forEach(function (item) {
+          if (item.checked === true) i++
+        })
+        return i
+      }
     }
   }
 }
